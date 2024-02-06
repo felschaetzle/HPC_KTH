32f4fd8799ed35084700c01428d7d19b2d88f32a
Thorben Fetz <14424-t.fetz@users.noreply.git.rwth-aachen.de>
2024-02-06 08:58:10 +0100

    start working on bonus exercise 2

---
+++ assignment2/docs/source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "DD2358 Conway's Game of Life Optimization"
+copyright = '2024, Group 17: Felix Schätzle, Georgios Mitsos, Thorben Fetz'
+author = 'Group 17: Felix Schätzle, Georgios Mitsos, Thorben Fetz'
+release = '2023-02-06'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'Python'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
