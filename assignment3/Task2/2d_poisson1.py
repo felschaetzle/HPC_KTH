a0d5b84f847ffce372e80f6d19c77693cfa3fafa
Georgios Mitsos <georgiosmitsos@Georgioss-MacBook-Air.local>
2024-02-14 00:16:33 +0100

    2.1,2.2,2.3 completed

---
+++ assignment3/Task2/2d_poisson1.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cythonfn_poisson
+
+def poisson_2d():
+    size = 64
+    x = np.random.rand(size,size)
+    
+    x[[-1,0],:] = 0 # boundary conditions
+    x[:,[-1,0]] = 0
+
+    for i in range(1000): # number of iterations
+        x = cythonfn_poisson.gauss_seidel(x)
+
+if __name__ == "__main__":
+    poisson_2d()
+    
+    
