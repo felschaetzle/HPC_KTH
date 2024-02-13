5a3eddf7eb948609d18cc42a4667e09765ef43cd
Thorben Fetz <14424-t.fetz@users.noreply.git.rwth-aachen.de>
2024-02-13 12:23:36 +0100

    finished development for 1.2

--- assignment3/cythonfn.pyx
+++ assignment3/cythonfn.pyx
@@ -1,16 +1,41 @@
+#cython: language_level=3
+from cython.cimports.libc.stdlib import malloc, free
 
-def copy(array_size, c, a):
+def copy(int array_size):
+    cdef float* a = <float*> malloc(array_size * sizeof(float))
+    cdef float* c = <float*> malloc(array_size * sizeof(float))
+    
     for j in range(array_size):
+        a[j] = 1.0
         c[j] = a[j]
 
-def sum(array_size, c, a, b):
+def sum(int array_size):
+    cdef float* a = <float*> malloc(array_size * sizeof(float))
+    cdef float* b = <float*> malloc(array_size * sizeof(float))
+    cdef float* c = <float*> malloc(array_size * sizeof(float))
+
+
     for j in range(array_size):
+        a[j] = 1.0
+        b[j] = 2.0
         c[j] = a[j] + b[j]
 
-def scale(array_size, b, scalar, c):
+def scale(int array_size):
+    cdef float* b = <float*> malloc(array_size * sizeof(float))
+    cdef float* c = <float*> malloc(array_size * sizeof(float))
+    cdef float scalar = 2.0
+
     for j in range(array_size):
+        b[j] = 2.0
         b[j] = scalar * c[j]
 
-def triad(array_size, a, b, scalar, c):
+def triad(int array_size):
+    cdef float* a = <float*> malloc(array_size * sizeof(float))
+    cdef float* b = <float*> malloc(array_size * sizeof(float))
+    cdef float* c = <float*> malloc(array_size * sizeof(float))
+    cdef float scalar = 2.0
+
     for j in range(array_size):
+        b[j] = 2.0
+        c[j] = 0.0
         a[j] = b[j] + scalar * c[j]
