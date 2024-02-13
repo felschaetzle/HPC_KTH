054b243e02abfd940178b33e7ba92d0c60e98a78
Thorben Fetz <14424-t.fetz@users.noreply.git.rwth-aachen.de>
2024-02-13 13:26:00 +0100

    documentation and streamlined code for 1.2

--- assignment3/cythonfn.pyx
+++ assignment3/cythonfn.pyx
@@ -1,41 +1,35 @@
 #cython: language_level=3
 from cython.cimports.libc.stdlib import malloc, free
 
-def copy(int array_size):
-    cdef float* a = <float*> malloc(array_size * sizeof(float))
-    cdef float* c = <float*> malloc(array_size * sizeof(float))
+cdef float* a
+cdef float* b
+cdef float* c
+cdef float scalar = 2.0
+
+def init_arrays(int array_size):
+    """initialize all float arrays with values"""
     
+    a = <float*> malloc(array_size * sizeof(float))
+    b = <float*> malloc(array_size * sizeof(float))
+    c = <float*> malloc(array_size * sizeof(float))
+
+    for i in range(array_size):
+        a[i] = 1.0
+        b[i] = 2.0
+        c[i] = 0.0
+
+def copy(int array_size):
     for j in range(array_size):
-        a[j] = 1.0
         c[j] = a[j]
 
 def sum(int array_size):
-    cdef float* a = <float*> malloc(array_size * sizeof(float))
-    cdef float* b = <float*> malloc(array_size * sizeof(float))
-    cdef float* c = <float*> malloc(array_size * sizeof(float))
-
-
     for j in range(array_size):
-        a[j] = 1.0
-        b[j] = 2.0
         c[j] = a[j] + b[j]
 
 def scale(int array_size):
-    cdef float* b = <float*> malloc(array_size * sizeof(float))
-    cdef float* c = <float*> malloc(array_size * sizeof(float))
-    cdef float scalar = 2.0
-
     for j in range(array_size):
-        b[j] = 2.0
         b[j] = scalar * c[j]
 
 def triad(int array_size):
-    cdef float* a = <float*> malloc(array_size * sizeof(float))
-    cdef float* b = <float*> malloc(array_size * sizeof(float))
-    cdef float* c = <float*> malloc(array_size * sizeof(float))
-    cdef float scalar = 2.0
-
     for j in range(array_size):
-        b[j] = 2.0
-        c[j] = 0.0
         a[j] = b[j] + scalar * c[j]
